84ecd76cc440394e8ff074a24d0d7f1302c78c1b
Satvik Sawhney <101198384+SawhneySatvik@users.noreply.github.com>
2024-03-14 22:54:48 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,1 +1,13 @@
+CREATE TABLE players(
+	player_id VARCHAR(10) PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+	nationality VARCHAR(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	"role" VARCHAR(50),
+	batting_style VARCHAR(50),
+	total_runs INT,
+	strike_rate DECIMAL(5,2),
+	bowling_style VARCHAR(50),
+	total_wickets SMALLINT
+);
 
